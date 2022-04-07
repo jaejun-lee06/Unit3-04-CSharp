54a58db420d88682a52c485afafdecd7132abec9
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-04-07 17:12:25 +0000

    done

--- main.cs
+++ main.cs
@@ -1,7 +1,25 @@
+// Created by: Jaejun Lee
+// Created on: Mar 2022
+//
+// This program accepts user input
 using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        double fahrenheit;
+        double celsius;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+        Console.WriteLine("This program converts Fahrenheit to Celsius");
+        Console.WriteLine("");
+        Console.Write("Enter the fahrenheit temperature(°F): ");
+        fahrenheit = double.Parse(Console.ReadLine());
+
+        celsius = (double)(fahrenheit - 32) * 5 / 9;
+
+        Console.WriteLine("");
+        Console.Write("The celsius temperature is: " + celsius.ToString("0.00") + " °C");
+        Console.ReadKey();
+        Console.WriteLine("\nDone.");
+    }
 }
